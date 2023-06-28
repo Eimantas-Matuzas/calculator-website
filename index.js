9561f9cd57e85545216d1b4e8562d3eeaf3ffdae
Eimantas-Matuzas <matuzaseimantas20030322+github@gmail.com>
2023-06-28 12:39:28 +0300

    deleted:    apie_mus.html 	deleted:    image forest.jpg 	modified:   index.html 	modified:   index.js 	modified:   kontaktai.html

--- index.js
+++ index.js
@@ -1,49 +1,101 @@
-jQuery(document).ready(function($){
+document.getElementById("calculate").addEventListener('click', function () {
+var voltage_val = "";
+var current_val = "";
+var resistance_val = "";
+var power_val = "";
 
-    $("#contact-form").submit(function(){
+var voltage_val =  voltage.value;
+var current_val = current.value;
+var resistance_val = resistance.value;
+var power_val = power.value;
 
-         $(".succes_msg").html('').hide();
-        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+// check if voltage is entered 
+if(voltage_val !== ""){
+// if yes {
+// check if current is entered
+if(current_val !== ""){
+// do corresponding formula (calculate resistance and power)
+var resistance_val = voltage_val / current_val;
+var power_val = voltage_val * current_val;
+}
+// if no{
+else {
+// check if resistance is entered
+if(resistance_val !== ""){
+// if yes {
+// do corresponding formula (calculate current and power)
+var current_val = voltage_val / resistance_val;
+var power_val = (voltage_val * voltage_val) / resistance_val ;
+}
+// if no {
+else {
+// check if power is entered
+if(power_val !== ""){
+// if yes {
+// do corresponding formula (calculate current and resistance)
+var current_val = power_val / voltage_val;
+var resistance_val = power_val / (current_val * current_val);
+}
+// if no{
+else {
+// // // // // // // // // // // // // // // // // // // // // // // // // echo 2 fields need to be filled
+}
+}
+}
+}
+// if no {
+else {
+// check if current is entered
+if(current_val !== ""){
+// if yes{
+// check if resistance is entered
+if(resistance_val !== ""){
+// if yes{
+// do correct formula (calculate voltage and power)
+var voltage_val = current_val * resistance_val;
+var power_val = (current_val * current_val) * resistance_val;
+}
+// if no {
+else {
+// check if power is entered
+if(power_val !== ""){
+// if yes {
+// do corresponding formula (calculate voltage and resistance)
+var voltage_val = power_val / current_val;
+var resistance_val = power_val / (current_val * current_val);
+}
+// if no {
+else {
+// // // // // // // // // // // // // // // // // // // // // // // // // echo 2 fields need to be filled
+}
+}
+}
+// if no {
+else {
+// check if resisrance is entered
+if(resistance_val !== ""){
+// if yes{
+// check if the power entered
+if(power_val !== ""){
+// if yes {
+// do corresponding formula (calculate voltage and current)
+var current_val = Math. sqrt(power_val / resistance_val) ;
+var voltage_val = Math. sqrt(power_val * resistance_val) ;
+}
+// if no {
+else {
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // echo 2 fields need to be filled
+}
+}
+}
+}
+voltage.value = voltage_val;
+current.value = current_val;
+resistance.value = resistance_val;
+power.value = power_val;});
+// add the ansers to the main page by editing the boxes the text was entered into
 
-        email_val = $("input[name=email]").val();
-        name_val = $("input[name=name]").val();
-        subject_val = $("input[name=subject]").val();
-        // message_val = $("input[name=message]").val();
-        // const message_val = document.getElementById('message');
-        message_val = $("#message").val()
-        errors = "";
-        $(".email-error").html(' Tavo paštas i kuri tikrai negausi spam').show();
-        $(".name-error").html('Tavo vardas').show();
-        $(".subject-error").html('Subject').show();
-        $(".message-error").html('Tavo nematomas pranešimas').show();
-        $(".succes_msg").html('').show();
+    // var text = document.getElementById('voltage');
+    // voltage.value = ' after clicking';
+// });
 
-        if(!email_val.match(mailformat))
-        {
-            errors = errors + "email";
-        $(".email-error").html(' Tavo paštas i kuri tikrai negausi spam <div class="text-danger">Jis būtinas</div>').show();
-        }
-
-        if(name_val == "")
-        {
-            errors = errors + "name";
-            $(".name-error").html('Tavo vardas <div class="text-danger">Jis būtinas</div>').show();
-        }
-        if(subject_val == "")
-        {
-            errors = errors + "subject";
-            $(".subject-error").html('Subject <div class="text-danger">Jis būtinas</div>').show();
-        }
-        if(message_val == "")
-        {
-            errors = errors + "message";
-            $(".message-error").html('Tavo nematomas pranešimas <div class="text-danger">Jis būtinas</div>').show();
-        }
-
-         if(errors == "")
-         {
-            $(".succes_msg").html('Žinutė sėkmingai išsisiuntė').show();
-         }
-        return false;
-    })
-});
